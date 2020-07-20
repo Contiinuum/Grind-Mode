3dfb41beb610da2362df079d3aa613f4e05ad501
Contiinuum <68207985+Contiinuum@users.noreply.github.com>
2020-07-21 00:13:58 +0200

    Add files via upload

--- src/Main.cs
+++ src/Main.cs
@@ -79,10 +79,10 @@ namespace AudicaModding
 
         public static class BuildInfo
         {
-            public const string Name = "IntroSkip";  // Name of the Mod.  (MUST BE SET)
+            public const string Name = "GrindMode";  // Name of the Mod.  (MUST BE SET)
             public const string Author = "Continuum"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
-            public const string Version = "0.2.0"; // Version of the Mod.  (MUST BE SET)
+            public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
         }
 
